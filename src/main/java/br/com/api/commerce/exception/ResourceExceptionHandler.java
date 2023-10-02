82063e7b0a44b677310124c8aa4c872ff38d7423
Israel Lima <israel_lima_16@hotmail.com>
2023-10-02 16:51:38 -0300

    Adicionar restricao script sql flyway e padronizar retorno nas exceptions lancadas

--- src/main/java/br/com/api/commerce/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/api/commerce/exception/ResourceExceptionHandler.java
@@ -1,7 +1,13 @@
 package br.com.api.commerce.exception;
 
-import org.springframework.http.HttpStatus;
+import java.util.List;
+import java.util.ArrayList;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -9,11 +15,36 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ResourceExceptionHandler {
 	
+	private MessageSource messageSource;
+
+    public ResourceExceptionHandler(MessageSource messageSource) {
+		this.messageSource = messageSource;
+	}
+	
 	@ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler({NotFoundException.class})
     public CampoInvalido handlerNotFoundException(NotFoundException exception) {
         CampoInvalido campoInvalido = new CampoInvalido(exception.getCampo(), exception.getMensagem());
         return campoInvalido;
     }
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public List<CampoInvalido> handlerArgumentNotValid(MethodArgumentNotValidException exception) {
+
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+        List<CampoInvalido> camposInvalidos = extrairErros(fieldErrors);
+        return camposInvalidos;
+    }
+	
+	 private List<CampoInvalido> extrairErros(List<FieldError> fieldErrors){
+	        List<CampoInvalido> camposInvalido = new ArrayList<>();
+	        fieldErrors.forEach(error -> {
+	            CampoInvalido fieldErro = new CampoInvalido(error.getField(),
+	                    messageSource.getMessage(error, LocaleContextHolder.getLocale()));
+	            camposInvalido.add(fieldErro);
+	        });
+	        return camposInvalido;
+	    }
 
 }
